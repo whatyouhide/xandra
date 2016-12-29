9bdd69acc1b17224ab6c144323232929bd6e98f4
Andrea Leopardi <an.leopardi@gmail.com>
2016-12-29 10:23:57 +0100

    Feedback

--- lib/xandra/type_parser.ex
+++ lib/xandra/type_parser.ex
@@ -1,9 +1,9 @@
 defmodule Xandra.TypeParser do
   @moduledoc false
 
-  # For now, this type parser doesn't support user-defined types.
+  # TODO: support user-defined types.
 
-  @basic_types ~w(
+  @builtin_types ~w(
     text
     blob
     ascii
@@ -37,39 +37,38 @@ defmodule Xandra.TypeParser do
     {[type], ""} =
       string
       |> String.downcase()
-      |> parse_with_acc(_acc = [])
+      |> parse_next(_acc = [])
     type
   end
 
-  for type <- @basic_types do
-    defp parse_with_acc(unquote(Atom.to_string(type)) <> rest, acc) do
-      parse_with_acc(rest, [unquote(type) | acc])
+  for type <- @builtin_types do
+    defp parse_next(unquote(Atom.to_string(type)) <> rest, acc) do
+      parse_next(rest, [unquote(type) | acc])
     end
   end
 
-  # Start of composite type
-  defp parse_with_acc("<" <> rest, acc) do
-    {children, rest} = parse_with_acc(rest, [])
-    [type | types] = acc
-    parse_with_acc(rest, [List.to_tuple([type | children]) | types])
+  # Start of composite type.
+  defp parse_next("<" <> rest, [type | types] = _acc) do
+    {children, rest} = parse_next(rest, [])
+    parse_next(rest, [List.to_tuple([type | children]) | types])
   end
 
   # One type finished, we just go on.
-  defp parse_with_acc(", " <> rest, acc) do
-    parse_with_acc(rest, acc)
+  defp parse_next(", " <> rest, acc) do
+    parse_next(rest, acc)
   end
 
   # Contents of composite type are finished, we return the children and the
   # rest.
-  defp parse_with_acc(">" <> rest, acc) do
+  defp parse_next(">" <> rest, acc) do
     {:lists.reverse(acc), rest}
   end
 
-  defp parse_with_acc("", acc) do
+  defp parse_next("", acc) do
     {:lists.reverse(acc), ""}
   end
 
-  defp parse_with_acc(other, _acc) do
+  defp parse_next(other, _acc) do
     raise ArgumentError, "unparsable type: #{inspect(other)}"
   end
 end
