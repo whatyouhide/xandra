5974779a57abfb70764acb51c46a3b7c5e298dd1
Andrea Leopardi <an.leopardi@gmail.com>
2016-12-29 14:27:24 +0100

    Impl manual

--- lib/xandra/type_parser.ex
+++ lib/xandra/type_parser.ex
@@ -30,45 +30,107 @@ defmodule Xandra.TypeParser do
     empty
     frozen
     varchar
-  )a
+  )
 
   @spec parse(String.t) :: atom | tuple
   def parse(string) do
-    {[type], ""} =
+    try do
       string
       |> String.downcase()
-      |> parse_next(_acc = [])
-    type
-  end
-
-  for type <- @builtin_types do
-    defp parse_next(unquote(Atom.to_string(type)) <> rest, acc) do
-      parse_next(rest, [unquote(type) | acc])
+      |> parse_node()
+      |> tree_to_simple_representation()
+    catch
+      :throw, {:error, message} when is_binary(message) ->
+        raise ArgumentError, "invalid type #{inspect(string)}: #{message}"
     end
   end
 
-  # Start of composite type.
-  defp parse_next("<" <> rest, [type | types] = _acc) do
-    {children, rest} = parse_next(rest, [])
-    parse_next(rest, [List.to_tuple([type | children]) | types])
+  defp tree_to_simple_representation(%{name: name, children: children}) when name in @builtin_types do
+    type = String.to_atom(name)
+
+    if children == [] do
+      type
+    else
+      List.to_tuple([type | Enum.map(children, &tree_to_simple_representation/1)])
+    end
   end
 
-  # One type finished, we just go on.
-  defp parse_next(", " <> rest, acc) do
-    parse_next(rest, acc)
+  defp tree_to_simple_representation(%{name: name} = _node) do
+    throw({:error, "unknown type #{inspect(name)}"})
   end
 
-  # Contents of composite type are finished, we return the children and the
-  # rest.
-  defp parse_next(">" <> rest, acc) do
-    {:lists.reverse(acc), rest}
+  @node %{
+    name: "",
+    children: [],
+    parent: nil,
+  }
+
+  def parse_node(string) do
+    parse_node(string, _current = @node)
   end
 
-  defp parse_next("", acc) do
-    {:lists.reverse(acc), ""}
+  # Starting the subtypes of the current type.
+  defp parse_node("<" <> rest, current_node) do
+    if current_node.name == "" do
+      throw({:error, "syntax error for children of empty type"})
+    else
+      first_child = %{@node | parent: current_node}
+      parse_node(rest, first_child)
+    end
   end
 
-  defp parse_next(other, _acc) do
-    raise ArgumentError, "unparsable type: #{inspect(other)}"
+  # One subtype of "parent" finished, on to the next.
+  defp parse_node("," <> rest, current_node) do
+    assert_parent_and_not_empty_subtype(current_node)
+    parent = current_node.parent
+    parent = append_child_to_node(parent, current_node)
+    next_child = %{@node | parent: parent}
+    parse_node(rest, next_child)
+  end
+
+  # Subtypes finished.
+  defp parse_node(">" <> rest, current_node) do
+    assert_parent_and_not_empty_subtype(current_node)
+    parent = current_node.parent
+    parent = append_child_to_node(parent, current_node)
+    parse_node(rest, parent)
+  end
+
+  # We skip spaces.
+  defp parse_node(" " <> rest, current_node) do
+    parse_node(rest, current_node)
+  end
+
+  defp parse_node(<<char, rest::binary>>, current_node) do
+    current_node = %{current_node | name: <<current_node.name::binary, char>>}
+    parse_node(rest, current_node)
+  end
+
+  defp parse_node("", current_node) do
+    case current_node do
+      %{name: ""} ->
+        throw({:error, "unexpected end of input"})
+      %{parent: parent} when not is_nil(parent) ->
+        throw({:error, "unexpected end of input"})
+      _other ->
+        current_node
+    end
+  end
+
+  defp append_child_to_node(%{children: children} = node, new_child) do
+    new_child = %{new_child | parent: nil}
+    %{node | children: children ++ [new_child]}
+  end
+
+  defp assert_parent_and_not_empty_subtype(%{parent: nil}) do
+    throw({:error, "syntax error for misplaced >"})
+  end
+
+  defp assert_parent_and_not_empty_subtype(%{name: ""}) do
+    throw({:error, "syntax error for empty subtype"})
+  end
+
+  defp assert_parent_and_not_empty_subtype(_node) do
+    :ok
   end
 end
--- test/xandra/type_parser_test.exs
+++ test/xandra/type_parser_test.exs
@@ -3,7 +3,7 @@ defmodule Xandra.TypeParserTest do
 
   import Xandra.TypeParser, only: [parse: 1]
 
-  test "parse/1" do
+  test "parse/1 with valid types" do
     assert parse("uuid") == :uuid
     assert parse("text") == :text
     assert parse("list<int>") == {:list, :int}
@@ -11,9 +11,40 @@ defmodule Xandra.TypeParserTest do
     assert parse("set<map<int, list<text>>>") == {:set, {:map, :int, {:list, :text}}}
     assert parse("map<list<int>, map<int, int>>") == {:map, {:list, :int}, {:map, :int, :int}}
 
+    # Spaces here and there
+    assert parse("map<int,text>") == {:map, :int, :text}
+    assert parse("map<list<int>,map<int,int>>") == {:map, {:list, :int}, {:map, :int, :int}}
+    assert parse(" map < int,   set < map < int, int  > > > ") == {:map, :int, {:set, {:map, :int, :int}}}
+
     # Mixed case
     assert parse("TEXT") == :text
     assert parse("MAP<INT, SET<TEXT>>") == {:map, :int, {:set, :text}}
     assert parse("List<tExT>") == {:list, :text}
+  end
+
+  test "parse/1 with invalid types" do
+    assert_raise ArgumentError, ~s(invalid type " ": unexpected end of input), fn ->
+      parse(" ")
+    end
+
+    assert_raise ArgumentError, ~s(invalid type "list<>": syntax error for empty subtype), fn ->
+      parse("list<>")
+    end
+
+    assert_raise ArgumentError, ~s(invalid type "list<int>>": syntax error for misplaced >), fn ->
+      parse("list<int>>")
+    end
+
+    assert_raise ArgumentError, ~s(invalid type "list<int,>": syntax error for empty subtype), fn ->
+      parse("list<int,>")
+    end
+
+    assert_raise ArgumentError, ~s(invalid type "list< ,int>": syntax error for empty subtype), fn ->
+      parse("list< ,int>")
+    end
+
+    assert_raise ArgumentError, ~s(invalid type "foo<bar>": unknown type "foo"), fn ->
+      parse("foo<bar>")
+    end
   end
 end
