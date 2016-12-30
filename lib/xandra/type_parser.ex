fcfe94765eaf597fa3b58be26ec9b3962097623a
Aleksei Magusev <lexmag@me.com>
2016-12-30 14:29:45 +0100

    Add TypeParser to convert CQL types to internal representation (#4)

---
+++ lib/xandra/type_parser.ex
@@ -0,0 +1,83 @@
+defmodule Xandra.TypeParser do
+  @moduledoc false
+
+  @builtin_types [
+    :text,
+    :blob,
+    :ascii,
+    :bigint,
+    :counter,
+    :int,
+    :varint,
+    :boolean,
+    :decimal,
+    :double,
+    :float,
+    :inet,
+    :timestamp,
+    :uuid,
+    :timeuuid,
+    :date,
+    :smallint,
+    :time,
+    :tinyint,
+    :map,
+    :set,
+    :list,
+    :tuple,
+    :empty,
+    :frozen,
+    :varchar,
+  ]
+
+  def parse(string) when is_binary(string) do
+    try do
+      string
+      |> String.replace(" ", "")
+      |> String.downcase()
+      |> parse_type()
+    catch
+      :throw, :invalid ->
+        raise "invalid type: " <> inspect(string)
+    else
+      {type, ""} ->
+        type
+      {_, _rest} ->
+        raise "invalid type: " <> inspect(string)
+    end
+  end
+
+  defp parse_type(chars) do
+    case parse_plain_type(chars) do
+      {type, "<" <> rest} ->
+        case parse_inner(rest) do
+          {types, ">" <> rest} ->
+            {{type, types}, rest}
+          _ ->
+            throw(:invalid)
+        end
+      {type, rest} ->
+        {type, rest}
+    end
+  end
+
+  for type <- @builtin_types do
+    defp parse_plain_type(unquote(Atom.to_string(type)) <> rest) do
+      {unquote(type), rest}
+    end
+  end
+
+  defp parse_plain_type(_other) do
+    throw(:invalid)
+  end
+
+  defp parse_inner(chars) do
+    case parse_type(chars) do
+      {type, "," <> rest} ->
+        {types, rest} = parse_inner(rest)
+        {[type | types], rest}
+      {type, rest} ->
+        {[type], rest}
+    end
+  end
+end
---
+++ test/xandra/type_parser_test.exs
@@ -0,0 +1,50 @@
+defmodule Xandra.TypeParserTest do
+  use ExUnit.Case, async: true
+
+  import Xandra.TypeParser, only: [parse: 1]
+
+  test "parse/1 with valid types" do
+    assert parse("uuid") == :uuid
+    assert parse("text") == :text
+    assert parse("list<int>") == {:list, [:int]}
+    assert parse("map<int, text>") == {:map, [:int, :text]}
+    assert parse("set<map<int, list<text>>>") == {:set, [{:map, [:int, {:list, [:text]}]}]}
+    assert parse("map<list<int>, map<int, int>>") == {:map, [{:list, [:int]}, {:map, [:int, :int]}]}
+
+    # Spaces here and there
+    assert parse("map<int,text>") == {:map, [:int, :text]}
+    assert parse("map<list<int>,map<int,int>>") == {:map, [{:list, [:int]}, {:map, [:int, :int]}]}
+    assert parse(" map < int,   set < map < int, int  > > > ") == {:map, [:int, {:set, [{:map, [:int, :int]}]}]}
+
+    # Deviant case
+    assert parse("TEXT") == :text
+    assert parse("MAP<INT, SET<TEXT>>") == {:map, [:int, {:set, [:text]}]}
+    assert parse("List<texT>") == {:list, [:text]}
+  end
+
+  test "parse/1 with invalid types" do
+    assert_raise RuntimeError, ~s(invalid type: " "), fn ->
+      parse(" ")
+    end
+
+    assert_raise RuntimeError, ~s(invalid type: "list<>"), fn ->
+      parse("list<>")
+    end
+
+    assert_raise RuntimeError, ~s(invalid type: "list<int>>"), fn ->
+      parse("list<int>>")
+    end
+
+    assert_raise RuntimeError, ~s(invalid type: "list<int,>"), fn ->
+      parse("list<int,>")
+    end
+
+    assert_raise RuntimeError, ~s(invalid type: "list< ,int>"), fn ->
+      parse("list< ,int>")
+    end
+
+    assert_raise RuntimeError, ~s(invalid type: "list<int>, int"), fn ->
+      parse("list<int>, int")
+    end
+  end
+end
