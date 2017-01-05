33808d9500a9059b8240d4b5fdb48c161334dde6
Aleksei Magusev <lexmag@me.com>
2017-01-05 17:59:51 +0100

    Introduce XandraTest.IntegrationCase

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,32 @@
 ExUnit.start()
+
+defmodule XandraTest.IntegrationCase do
+  use ExUnit.CaseTemplate
+
+  using [keyspace: keyspace] do
+    quote do
+      setup_all do
+        keyspace = "xandra_test_" <> unquote(keyspace)
+        unquote(__MODULE__).setup_keyspace(keyspace)
+
+        %{keyspace: keyspace}
+      end
+    end
+  end
+
+  setup %{keyspace: keyspace} do
+    {:ok, conn} = Xandra.start_link()
+    {:ok, _void} = Xandra.execute(conn, "USE #{keyspace}", [])
+    %{conn: conn}
+  end
+
+  def setup_keyspace(keyspace) do
+    {:ok, conn} = Xandra.start_link()
+    {:ok, _void} = Xandra.execute(conn, "DROP KEYSPACE IF EXISTS #{keyspace}", [])
+    statement = """
+    CREATE KEYSPACE #{keyspace}
+    WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1}
+    """
+    {:ok, _void} = Xandra.execute(conn, statement, [])
+  end
+end
