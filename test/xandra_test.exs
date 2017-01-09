ac27a94906b63db34deec8f3f17413024bdefdba
Andrea Leopardi <an.leopardi@gmail.com>
2017-01-09 15:55:40 +0100

    Feedback

--- test/xandra_test.exs
+++ test/xandra_test.exs
@@ -2,14 +2,14 @@ defmodule XandraTest do
   use ExUnit.Case, async: true
 
   test "options validation in Xandra.start_link/1" do
-    message = "expected an integer as the value of the :port option, got: :not_an_integer"
+    message = "expected an integer as the value of the :port option, got: :invalid"
     assert_raise ArgumentError, message, fn ->
-      Xandra.start_link(port: :not_an_integer)
+      Xandra.start_link(port: :invalid)
     end
 
-    message = "expected a string as the value of the :host option, got: :not_a_string"
+    message = "expected a string as the value of the :host option, got: :invalid"
     assert_raise ArgumentError, message, fn ->
-      Xandra.start_link(host: :not_a_string)
+      Xandra.start_link(host: :invalid)
     end
   end
 end
