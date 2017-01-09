ad94644f22267941edb21a64100563f859af8aeb
Aleksei Magusev <lexmag@me.com>
2017-01-09 16:39:41 +0100

    Rework Error response (#22)

---
+++ test/errors_test.exs
@@ -0,0 +1,21 @@
+defmodule ErrorsTest do
+  use XandraTest.IntegrationCase
+
+  alias Xandra.Error
+
+  test "each possible error", %{conn: conn} do
+    assert {:error, reason} = Xandra.execute(conn, "", [])
+    assert %Error{reason: :invalid_syntax} = reason
+
+    assert {:error, reason} = Xandra.execute(conn, "USE unknown", [])
+    assert %Error{reason: :invalid} = reason
+
+    {:ok, _void} = Xandra.execute(conn, "CREATE TABLE errors (id int PRIMARY KEY)", [])
+    assert {:error, reason} = Xandra.execute(conn, "CREATE TABLE errors (id int PRIMARY KEY)", [])
+    assert %Error{reason: :already_exists} = reason
+
+    {:ok, query} = Xandra.prepare(conn, "SELECT * FROM errors", [])
+    assert {:error, reason} = Xandra.execute(conn, %{query | id: <<>>}, [])
+    assert %Error{reason: :unprepared} = reason
+  end
+end
