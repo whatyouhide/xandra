5c6cc5b71f687a4c633cbef59be702d88392fc7d
Andrea Leopardi <an.leopardi@gmail.com>
2017-01-11 12:48:49 +0100

    Bump the Elixir requirement to ~> 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Xandra.Mixfile do
   def project() do
     [app: :xandra,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
