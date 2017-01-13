79bbd916cd138858da2666b1e7d171019007a6c1
Andrea Leopardi <an.leopardi@gmail.com>
2017-01-13 15:39:30 +0100

    Don't automatically prepare queries when creating a stream

--- lib/xandra.ex
+++ lib/xandra.ex
@@ -18,13 +18,11 @@ defmodule Xandra do
   def stream!(conn, query, params, options \\ [])
 
   def stream!(conn, statement, params, options) when is_binary(statement) do
-    with {:ok, query} <- prepare(conn, statement, options) do
-      stream!(conn, query, params, options)
-    end
+    %Stream{conn: conn, query: statement, params: params, options: options}
   end
 
-  def stream!(conn, %Prepared{} = query, params, options) do
-    %Xandra.Stream{conn: conn, query: query, params: params, options: options}
+  def stream!(conn, %Prepared{} = prepared, params, options) do
+    %Stream{conn: conn, query: prepared, params: params, options: options}
   end
 
   def prepare(conn, statement, options \\ []) when is_binary(statement) do
