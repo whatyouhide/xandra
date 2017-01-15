6bd978e14a792581082ce910c74f697435498b39
Andrea Leopardi <an.leopardi@gmail.com>
2017-01-15 23:43:31 +0100

    Get mix.exs ready for release on Hex

--- mix.exs
+++ mix.exs
@@ -1,17 +1,41 @@
 defmodule Xandra.Mixfile do
   use Mix.Project
 
+  @description """
+  Fast, simple, and robust Cassandra driver for Elixir.
+  """
+
+  @repo_url "https://github.com/lexhide/xandra"
+
+  @version "0.0.1"
+
   def project() do
     [app: :xandra,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+
+     # Hex
+     package: package(),
+     description: @description,
+
+     # Docs
+     name: "Xandra",
+     docs: [main: "Xandra",
+            source_ref: "v#{@version}",
+            source_url: @repo_url]]
   end
 
   def application() do
     [applications: [:logger, :db_connection]]
+  end
+
+  defp package() do
+    [maintainers: ["Aleksei Magusev", "Andrea Leopardi"],
+     licenses: ["ISC"],
+     links: %{"GitHub" => @repo_url}]
   end
 
   defp deps() do
