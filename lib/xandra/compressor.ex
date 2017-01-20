31abc504eca1c6a90e1daedc82aa41824114e8e2
Andrea Leopardi <an.leopardi@gmail.com>
2017-01-20 14:14:36 +0100

    Docs

--- lib/xandra/compressor.ex
+++ lib/xandra/compressor.ex
@@ -1,20 +1,57 @@
 defmodule Xandra.Compressor do
   @moduledoc """
-  TODOTODO
+  A behaviour to compress and decompress binary data.
+
+  Modules implementing this behaviour can be used to compress and uncompress
+  data using one of the compression algorithms supported by Cassandra (as of
+  now, lz4 and snappy).
+
+  ## Example
+
+  Let's imagine you implemented the snappy compression algorithm in your
+  application:
+
+      defmodule Snappy do
+        def compress(binary), do: # ...
+        def uncompress(binary), do: # ...
+      end
+
+  You can then implement a module that implements the `Xandra.Compressor`
+  behaviour and can be used to compress and uncompress data flowing through the
+  connection to Cassandra:
+
+      defmodule SnappyXandraCompressor do
+        @behaviour Xandra.Compressor
+
+        def algorithm(), do: :snappy
+        defdelegate compress(binary), to: Snappy
+        defdelegate uncompress(binary), to: Snappy
+      end
+
+  Now, this module can be passed as the value of the `:compressor` option to
+  many functions in `Xandra`:
+
+      Xandra.start_link(compressor: SnappyXandraCompressor)
+
+  For more information on compression, see the "Compression" section in the
+  documentation for `Xandra.`
   """
 
   @doc """
-  TODO
+  Specifies which algorithm this module will use to compress and uncompress
+  data.
   """
   @callback algorithm() :: :lz4 | :snappy
 
   @doc """
-  TODO
+  Compresses the given binary according to the algorithm returned by
+  `c:algorithm/0`.
   """
   @callback compress(binary) :: binary
 
   @doc """
-  TODO
+  Compresses the given binary according to the algorithm returned by
+  `c:algorithm/0`.
   """
   @callback decompress(binary) :: binary
 end
