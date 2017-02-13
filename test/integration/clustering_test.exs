df4dbe5d802b45d2699c2e119cd1ff0ba3a6e192
Aleksei Magusev <lexmag@me.com>
2017-02-13 18:03:25 +0100

    WIP

--- test/integration/clustering_test.exs
+++ test/integration/clustering_test.exs
@@ -6,12 +6,15 @@ defmodule ClusteringTest do
   @call_options [pool: Xandra.Cluster]
 
   test "basic interactions" do
-    nodes = ["127.0.0.1", "127.0.0.1", "127.0.0.2"]
-    {:ok, cluster} = Xandra.start_link(@call_options ++ [nodes: nodes])
     log = capture_log(fn ->
+      start_options = [nodes: ["127.0.0.1", "127.0.0.1", "127.0.0.2"], name: TestCluster]
+      {:ok, cluster} = Xandra.start_link(@call_options ++ start_options)
+
       assert await_available(cluster) == Xandra.execute!(cluster, "USE system", [], @call_options)
     end)
     assert log =~ "received request to start another connection pool to the same address"
+
+    assert Xandra.execute!(TestCluster, "USE system", [], @call_options)
   end
 
   defp await_available(cluster, tries \\ 4) do
