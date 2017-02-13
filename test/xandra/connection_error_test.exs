a0e4bd9c7c3a260b6c6e0afd4e89c852c75e5c60
Aleksei Magusev <lexmag@me.com>
2017-02-13 21:03:52 +0100

    Rename Xandra.Connection.Error –> Xandra.ConnectionError

---
+++ test/xandra/connection_error_test.exs
@@ -0,0 +1,10 @@
+defmodule Xandra.ConnectionErrorTest do
+  use ExUnit.Case, async: true
+
+  alias Xandra.ConnectionError
+
+  test "message/1" do
+    error = ConnectionError.new("connect", :econnrefused)
+    assert ConnectionError.message(error) == "action \"connect\" failed with reason: connection refused"
+  end
+end
