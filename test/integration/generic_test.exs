a8ff680678da333cc91459c9d00596523bc19bb1
Andrea Leopardi <an.leopardi@gmail.com>
2017-02-28 23:48:49 +0100

    update tests

--- test/integration/generic_test.exs
+++ test/integration/generic_test.exs
@@ -2,6 +2,6 @@ defmodule GenericTest do
   use XandraTest.IntegrationCase, async: true
 
   test "Xandra.run/3", %{conn: conn, keyspace: keyspace} do
-    assert {:ok, _} = Xandra.run(conn, [], fn conn -> Xandra.execute(conn, "USE #{keyspace}", []) end)
+    assert %Xandra.SetKeyspace{} = Xandra.run(conn, [], &Xandra.execute!(&1, "USE #{keyspace}", []))
   end
 end
