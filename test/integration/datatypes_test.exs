953e2b8d7a011778e6644e5ed5ea2e68b7b8e3c7
Andrea Leopardi <an.leopardi@gmail.com>
2017-03-06 12:10:53 +0100

    Make datatypes-related tests stronger
    If we test fields in rows by using the access syntax ("row[field]"), we don't
    really test that 1) the row is a map and that 2) the field is present if we test
    for "== nil". "Map.fetch!/2" solves this.

--- test/integration/datatypes_test.exs
+++ test/integration/datatypes_test.exs
@@ -26,22 +26,22 @@ defmodule DataTypesTest do
     Xandra.execute!(conn, "INSERT INTO primitives (id) VALUES (1)", [])
     page = Xandra.execute!(conn, "SELECT * FROM primitives WHERE id = 1", [])
     assert [row] = Enum.to_list(page)
-    assert row["id"] == 1
-    assert row["ascii"] == nil
-    assert row["bigint"] == nil
-    assert row["blob"] == nil
-    assert row["boolean"] == nil
-    assert row["decimal"] == nil
-    assert row["double"] == nil
-    assert row["float"] == nil
-    assert row["inet"] == nil
-    assert row["int"] == nil
-    assert row["text"] == nil
-    assert row["timestamp"] == nil
-    assert row["timeuuid"] == nil
-    assert row["uuid"] == nil
-    assert row["varchar"] == nil
-    assert row["varint"] == nil
+    assert Map.fetch!(row, "id") == 1
+    assert Map.fetch!(row, "ascii") == nil
+    assert Map.fetch!(row, "bigint") == nil
+    assert Map.fetch!(row, "blob") == nil
+    assert Map.fetch!(row, "boolean") == nil
+    assert Map.fetch!(row, "decimal") == nil
+    assert Map.fetch!(row, "double") == nil
+    assert Map.fetch!(row, "float") == nil
+    assert Map.fetch!(row, "inet") == nil
+    assert Map.fetch!(row, "int") == nil
+    assert Map.fetch!(row, "text") == nil
+    assert Map.fetch!(row, "timestamp") == nil
+    assert Map.fetch!(row, "timeuuid") == nil
+    assert Map.fetch!(row, "uuid") == nil
+    assert Map.fetch!(row, "varchar") == nil
+    assert Map.fetch!(row, "varint") == nil
 
     statement = """
     INSERT INTO primitives
@@ -84,21 +84,21 @@ defmodule DataTypesTest do
     Xandra.execute!(conn, statement, values)
     page = Xandra.execute!(conn, "SELECT * FROM primitives WHERE id = 2", [])
     assert [row] = Enum.to_list(page)
-    assert row["ascii"] == "ascii"
-    assert row["bigint"] == 1000000000
-    assert row["blob"] == <<0, 0xFF>>
-    assert row["boolean"] == true
-    assert row["decimal"] == {1323, 2}
-    assert row["double"] == 3.1415
-    assert row["float"] == -1.25
-    assert row["inet"] == {192, 168, 0, 1}
-    assert row["int"] == 42
-    assert row["text"] == "эликсир"
-    assert row["timestamp"] == 2167219200
-    assert row["timeuuid"] == <<254, 43, 67, 96, 40, 198, 17, 226, 129, 193, 8, 0, 32, 12, 154, 102>>
-    assert row["uuid"] == <<0, 182, 145, 128, 208, 225, 17, 226, 139, 139, 8, 0, 32, 12, 154, 102>>
-    assert row["varchar"] == "тоже эликсир"
-    assert row["varint"] == 6789065678192312391879827349
+    assert Map.fetch!(row, "ascii") == "ascii"
+    assert Map.fetch!(row, "bigint") == 1000000000
+    assert Map.fetch!(row, "blob") == <<0, 0xFF>>
+    assert Map.fetch!(row, "boolean") == true
+    assert Map.fetch!(row, "decimal") == {1323, 2}
+    assert Map.fetch!(row, "double") == 3.1415
+    assert Map.fetch!(row, "float") == -1.25
+    assert Map.fetch!(row, "inet") == {192, 168, 0, 1}
+    assert Map.fetch!(row, "int") == 42
+    assert Map.fetch!(row, "text") == "эликсир"
+    assert Map.fetch!(row, "timestamp") == 2167219200
+    assert Map.fetch!(row, "timeuuid") == <<254, 43, 67, 96, 40, 198, 17, 226, 129, 193, 8, 0, 32, 12, 154, 102>>
+    assert Map.fetch!(row, "uuid") == <<0, 182, 145, 128, 208, 225, 17, 226, 139, 139, 8, 0, 32, 12, 154, 102>>
+    assert Map.fetch!(row, "varchar") == "тоже эликсир"
+    assert Map.fetch!(row, "varint") == 6789065678192312391879827349
   end
 
   test "collection datatypes", %{conn: conn} do
@@ -115,11 +115,11 @@ defmodule DataTypesTest do
     Xandra.execute!(conn, "INSERT INTO collections (id) VALUES (1)", [])
     page = Xandra.execute!(conn, "SELECT * FROM collections WHERE id = 1", [])
     assert [row] = Enum.to_list(page)
-    assert row["id"] == 1
-    assert row["list_of_int"] == nil
-    assert row["map_of_int_to_text"] == nil
-    assert row["set_of_int"] == nil
-    assert row["tuple_of_int_and_text"] == nil
+    assert Map.fetch!(row, "id") == 1
+    assert Map.fetch!(row, "list_of_int") == nil
+    assert Map.fetch!(row, "map_of_int_to_text") == nil
+    assert Map.fetch!(row, "set_of_int") == nil
+    assert Map.fetch!(row, "tuple_of_int_and_text") == nil
 
     # Empty collections
     statement = """
@@ -140,10 +140,10 @@ defmodule DataTypesTest do
     Xandra.execute!(conn, statement, values)
     page = Xandra.execute!(conn, "SELECT * FROM collections WHERE id = 1", [])
     assert [row] = Enum.to_list(page)
-    assert row["id"] == 1
-    assert row["list_of_int"] == nil
-    assert row["map_of_int_to_text"] == nil
-    assert row["set_of_int"] == nil
+    assert Map.fetch!(row, "id") == 1
+    assert Map.fetch!(row, "list_of_int") == nil
+    assert Map.fetch!(row, "map_of_int_to_text") == nil
+    assert Map.fetch!(row, "set_of_int") == nil
 
     # Collections with items in them
     statement = """
@@ -165,10 +165,10 @@ defmodule DataTypesTest do
     Xandra.execute!(conn, statement, values)
     page = Xandra.execute!(conn, "SELECT * FROM collections WHERE id = 1", [])
     assert [row] = Enum.to_list(page)
-    assert row["id"] == 1
-    assert row["list_of_int"] == [24, 42]
-    assert row["map_of_int_to_text"] == %{24 => "24", 42 => "42"}
-    assert row["set_of_int"] == MapSet.new([42, 24])
-    assert row["tuple_of_int_and_text"] == {24, "42"}
+    assert Map.fetch!(row, "id") == 1
+    assert Map.fetch!(row, "list_of_int") == [24, 42]
+    assert Map.fetch!(row, "map_of_int_to_text") == %{24 => "24", 42 => "42"}
+    assert Map.fetch!(row, "set_of_int") == MapSet.new([42, 24])
+    assert Map.fetch!(row, "tuple_of_int_and_text") == {24, "42"}
   end
 end
