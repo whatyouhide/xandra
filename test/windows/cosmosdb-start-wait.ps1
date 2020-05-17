85306ab60803dfafef7558f7a14592bba14daf02
p2k <me@p2k-network.org>
2020-05-17 19:26:39 +0200

    Working Windows CI tests

--- test/windows/cosmosdb-start-wait.ps1
+++ test/windows/cosmosdb-start-wait.ps1
@@ -1,12 +1,21 @@
 
 Push-Location -Path "$Env:Programfiles\Azure Cosmos DB Emulator"
 
-Start-Process -FilePath ".\Microsoft.Azure.Cosmos.Emulator.exe" -Args "/EnableCassandraEndpoint","/EnableRateLimiting","/NoExplorer"
+Start-Process -FilePath ".\Microsoft.Azure.Cosmos.Emulator.exe" -Args "/EnableCassandraEndpoint","/EnableRateLimiting","/NoExplorer","/NoUI"
 
 Write-Host "Waiting for Cosmos DB to start..."
-do {
+$lastExitCode = 0
+Do {
   Start-Sleep -Seconds 1
   $process = (Start-Process -FilePath ".\Microsoft.Azure.Cosmos.Emulator.exe" -Args "/GetStatus" -PassThru -Wait)
-} while($process.ExitCode -ne 2)
+  If ($process.ExitCode -ne $lastExitCode) {
+    Write-Host "GetStatus code =" $process.ExitCode
+    $lastExitCode = $process.ExitCode
+  }
+  If ($lastExitCode -eq 3 -or $lastExitCode -lt 0) {
+    Pop-Location
+    Exit 1
+  }
+} While ($process.ExitCode -ne 2)
 
 Pop-Location
