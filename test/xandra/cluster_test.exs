2f238d6a63ca5b32fba94cecab2aedf8b35e418c
Hubert Pompecki <hpompecki@gmail.com>
2022-03-23 17:04:02 +0100

    Add tests for dc-aware load balancing

--- test/xandra/cluster_test.exs
+++ test/xandra/cluster_test.exs
@@ -8,100 +8,128 @@ defmodule Xandra.ClusterTest do
 
   @moduletag capture_log: true
 
-  test "connects to new node in same datacenter when autodiscover_other_datacenters is false" do
-    {:ok, cluster} =
-      Cluster.start_link(
-        nodes: ["127.0.0.1"],
-        autodiscover_other_datacenters: false,
-        autodiscovery: true
-      )
-
-    wait_for_connection(cluster)
-
-    log =
-      capture_log(fn ->
-        Cluster.update(
-          cluster,
-          %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
-          "datacenter1"
+  describe "autodiscover_other_datacenters" do
+    test "connects to new node in same datacenter when autodiscover_other_datacenters is false" do
+      {:ok, cluster} =
+        Cluster.start_link(
+          nodes: ["127.0.0.1"],
+          autodiscover_other_datacenters: false,
+          autodiscovery: true
         )
 
-        wait_for_connection(cluster)
-      end)
+      wait_for_connection(cluster)
 
-    assert log =~ "Started connection to {192, 0, 2, 0}"
+      log =
+        capture_log(fn ->
+          Cluster.update(
+            cluster,
+            %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
+            "datacenter1"
+          )
+
+          wait_for_connection(cluster)
+        end)
+
+      assert log =~ "Started connection to {192, 0, 2, 0}"
+    end
+
+    test "doesn't connect to new node in different datacenter when autodiscover_other_datacenters is false" do
+      {:ok, cluster} =
+        Cluster.start_link(
+          nodes: ["127.0.0.1"],
+          autodiscover_other_datacenters: false,
+          autodiscovery: true
+        )
+
+      wait_for_connection(cluster)
+
+      log =
+        capture_log(fn ->
+          Cluster.update(
+            cluster,
+            %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
+            "datacenter2"
+          )
+
+          wait_for_connection(cluster)
+        end)
+
+      refute log =~ "Started connection to {192, 0, 2, 0}"
+    end
+
+    test "connects to new node in same datacenter when autodiscover_other_datacenters is true" do
+      {:ok, cluster} =
+        Cluster.start_link(
+          nodes: ["127.0.0.1"],
+          autodiscover_other_datacenters: true,
+          autodiscovery: true
+        )
+
+      wait_for_connection(cluster)
+
+      log =
+        capture_log(fn ->
+          Cluster.update(
+            cluster,
+            %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
+            "datacenter1"
+          )
+
+          wait_for_connection(cluster)
+        end)
+
+      assert log =~ "Started connection to {192, 0, 2, 0}"
+    end
+
+    test "connects to new node in different datacenter when autodiscover_other_datacenters is true" do
+      {:ok, cluster} =
+        Cluster.start_link(
+          nodes: ["127.0.0.1"],
+          autodiscover_other_datacenters: true,
+          autodiscovery: true
+        )
+
+      wait_for_connection(cluster)
+
+      log =
+        capture_log(fn ->
+          Cluster.update(
+            cluster,
+            %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
+            "datacenter2"
+          )
+
+          wait_for_connection(cluster)
+        end)
+
+      assert log =~ "Started connection to {192, 0, 2, 0}"
+    end
   end
 
-  test "doesn't connect to new node in different datacenter when autodiscover_other_datacenters is false" do
-    {:ok, cluster} =
-      Cluster.start_link(
-        nodes: ["127.0.0.1"],
-        autodiscover_other_datacenters: false,
-        autodiscovery: true
-      )
-
-    wait_for_connection(cluster)
-
-    log =
-      capture_log(fn ->
-        Cluster.update(
-          cluster,
-          %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
-          "datacenter2"
+  describe "dc-aware load balancing" do
+    test "prefers first node's data center" do
+      {:ok, cluster} =
+        Cluster.start_link(
+          nodes: ["127.0.0.1"],
+          autodiscover_other_datacenters: true,
+          autodiscovery: true,
+          load_balancing: :dc_aware
         )
 
-        wait_for_connection(cluster)
-      end)
+      wait_for_connection(cluster)
 
-    refute log =~ "Started connection to {192, 0, 2, 0}"
-  end
+      {:ok, inital_pool} = GenServer.call(cluster, :checkout)
 
-  test "connects to new node in same datacenter when autodiscover_other_datacenters is true" do
-    {:ok, cluster} =
-      Cluster.start_link(
-        nodes: ["127.0.0.1"],
-        autodiscover_other_datacenters: true,
-        autodiscovery: true
+      Cluster.update(
+        cluster,
+        %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
+        "datacenter2"
       )
 
-    wait_for_connection(cluster)
+      wait_for_connection(cluster)
 
-    log =
-      capture_log(fn ->
-        Cluster.update(
-          cluster,
-          %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
-          "datacenter1"
-        )
-
-        wait_for_connection(cluster)
-      end)
-
-    assert log =~ "Started connection to {192, 0, 2, 0}"
-  end
-
-  test "connects to new node in different datacenter when autodiscover_other_datacenters is true" do
-    {:ok, cluster} =
-      Cluster.start_link(
-        nodes: ["127.0.0.1"],
-        autodiscover_other_datacenters: true,
-        autodiscovery: true
-      )
-
-    wait_for_connection(cluster)
-
-    log =
-      capture_log(fn ->
-        Cluster.update(
-          cluster,
-          %TopologyChange{effect: "NEW_NODE", address: {192, 0, 2, 0}, port: 9042},
-          "datacenter2"
-        )
-
-        wait_for_connection(cluster)
-      end)
-
-    assert log =~ "Started connection to {192, 0, 2, 0}"
+      assert {:ok, ^inital_pool} = GenServer.call(cluster, :checkout)
+    end
   end
 
   defp wait_for_connection(cluster) do
