c0e6384489b9fa81b364af3c726651d22e9505f7
Andrea leopardi <an.leopardi@gmail.com>
2022-05-10 17:15:24 +0200

    Maybe fix

--- test/docker/health-check-services.sh
+++ test/docker/health-check-services.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# $SECONDS contains the number of seconds elapsed since starting the script.
+END_SECONDS=$((SECONDS+120))
+
 for name in $(docker ps --format '{{.Names}}'); do
-  while true; do
-    STATUS=$(docker inspect --format "{{json .State.Health.Status }}" $name)
+  while [[ "$SECONDS" -lt "$END_SECONDS" ]]; do
+    STATUS=$(docker inspect --format "{{json .State.Health.Status }}" "$name")
+
     if [[ $STATUS == "\"healthy\"" ]]; then
       echo "Docker $name is up and running"
       break
     fi
+
     >&2 echo "Docker $name is unavailable, waiting to start...";
     sleep 3;
   done
