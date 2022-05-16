c84fb2ed9d5ea59683efc5a86897bf04f5246754
Andrea leopardi <an.leopardi@gmail.com>
2022-05-16 11:46:16 +0200

    Maybe this

--- test/integration/encryption_test.exs
+++ test/integration/encryption_test.exs
@@ -6,6 +6,11 @@ defmodule EncryptionTest do
 
   use XandraTest.IntegrationCase, start_options: start_options
 
+  # TODO: unskip when we figure out how the heck to run C* 4 with encryption.
+  if String.starts_with?(System.get_env("CASSANDRA_VERSION", ""), "4") do
+    @moduletag :skip
+  end
+
   @moduletag :encryption
 
   test "encrypted connections", %{keyspace: keyspace, start_options: start_options} do
