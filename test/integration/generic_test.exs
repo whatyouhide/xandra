df6eddafa4abf22f4c121665165538622c3d9159
Andrea leopardi <an.leopardi@gmail.com>
2023-01-20 15:00:21 +0100

    Start actually using :default_consistency
    Closes #266.
    
    There was a bug preventing the use of the ":default_consistency" option,
    since we were setting ":consistency" to ":one" as a default, never
    actually falling back to ":default_consistency". With this commit,
    ":consistency" defaults to not being present instead.

--- test/integration/generic_test.exs
+++ test/integration/generic_test.exs
@@ -16,4 +16,13 @@ defmodule GenericTest do
              %{"code" => 1, "name" => "Meg"}
            ]
   end
+
+  # Regression for https://github.com/lexhide/xandra/issues/266
+  # This test doesn't test much for now, it's sort of a smoke test. Once we'll have
+  # generic telemetry events for queries, we can change this test to assert on the telemetry
+  # event.
+  test ":default_consistency option when starting", %{keyspace: keyspace} do
+    assert {:ok, test_conn} = start_supervised({Xandra, default_consistency: :three})
+    Xandra.execute!(test_conn, "USE #{keyspace}")
+  end
 end
