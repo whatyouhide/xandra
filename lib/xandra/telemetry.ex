e06cadec49ac44e298f3f99b344cf300f2d3b555
Harun Zengin <h.zengin@sonnen.de>
2023-03-30 15:54:55 +0200

    add default handler to telemetry

--- lib/xandra/telemetry.ex
+++ lib/xandra/telemetry.ex
@@ -6,6 +6,8 @@ defmodule Xandra.Telemetry do
   metrics and events. Below we list all the possible events emitted by Xandra, alongside
   their measurements and metadata.
 
+  Xandra emits telemetry events *since v0.15.0*.
+
   ### Xandra Connection
 
   * `[:xandra, :connection]` and `[:xandra, :disconnection]`
@@ -13,7 +15,7 @@ defmodule Xandra.Telemetry do
     * Measurements:
 
     * Metadata:
-      * `:connection_name` - given name of the connection or `nil`
+      * `:connection_name` - given name of the connection or `nil` if not set
       * `:host` - the host of the node the connection is connected to
       * `:port` - the port of the node the connection is connected to
       * `:reason` - reason of disconnection
@@ -26,7 +28,7 @@ defmodule Xandra.Telemetry do
 
     * Metadata:
       * `:query` - the `t:Xandra.Prepared.t/0` query
-      * `:connection_name` - given name of the connection or `nil`
+      * `:connection_name` - given name of the connection or `nil` if not set
       * `:host` - the host of the node the connection is connected to
       * `:port` - the port of the node the connection is connected to
       * `:reason` - if error, reason
@@ -39,7 +41,7 @@ defmodule Xandra.Telemetry do
 
     * Metadata:
       * `:query` - the `t:Xandra.Simple.t/0` or `t:Xandra.Batch.t/0` query
-      * `:connection_name` - given name of the connection or `nil`
+      * `:connection_name` - given name of the connection or `nil` if not set
       * `:host` - the host of the node the connection is connected to
       * `:port` - the port of the node the connection is connected to
       * `:reason` - if error, reason
@@ -48,8 +50,6 @@ defmodule Xandra.Telemetry do
 
     * Measurements:
       * `:query` - the `t:Xandra.Prepared.t/0` query
-
-  Xandra emits telemetry events *since v0.15.0*.
 
   ### Warnings
 
@@ -74,6 +74,17 @@ defmodule Xandra.Telemetry do
 
   require Logger
 
+  @doc """
+  Attaches a handler that logs the given events:
+
+  `[:xandra, :connection]` and  `[:xandra, :disconnection]` - logged at info level
+  `[:xandra, :prepared_cache, :hit]` and `[:xandra, :prepared_cache, :miss]` - logged at debug level
+  `[:xandra, :prepare_query, :start]` and `[:xandra, :prepare_query, :stop]` - logged at debug level
+  `[:xandra, :prepare_query, :exception]` - logged at exception level
+  `[:xandra, :execute_query, :start]` and `[:xandra, :execute_query, :stop]` - logged at debug level
+  `[:xandra, :execute_query, :exception]` - logged at exception level
+  `[:xandra, :server_warnings]` - logged at warn level
+  """
   def attach_default_handler() do
     events = [
       [:xandra, :connection],
@@ -82,8 +93,11 @@ defmodule Xandra.Telemetry do
       [:xandra, :prepared_cache, :miss],
       [:xandra, :prepare_query, :start],
       [:xandra, :prepare_query, :stop],
+      [:xandra, :prepare_query, :exception],
       [:xandra, :execute_query, :start],
-      [:xandra, :execute_query, :stop]
+      [:xandra, :execute_query, :stop],
+      [:xandra, :execute_query, :exception],
+      [:xandra, :server_warnings]
     ]
 
     :telemetry.attach_many(
@@ -94,7 +108,74 @@ defmodule Xandra.Telemetry do
     )
   end
 
+  @spec handle_event(nonempty_maybe_improper_list, any, any, :no_config) :: :ok
   def handle_event([:xandra | event], measurements, metadata, :no_config) do
-    :ok
+    case event do
+      [:connection] ->
+        Logger.info(
+          "Connection established to #{metadata.host}:#{metadata.port} with name: " <>
+            "#{metadata.connection_name}"
+        )
+
+      [:disconnection] ->
+        Logger.info(
+          "Disconnected from #{metadata.host}:#{metadata.port} with name: " <>
+            "#{metadata.connection_name}. Reason: #{inspect(metadata.reason)}"
+        )
+
+      [:server_warnings] ->
+        # TODO incompatible names
+        Logger.warn(
+          "Received warning from #{metadata.address}:#{metadata.port}, " <>
+            "warnings: #{inspect(measurements.warnings)}"
+        )
+
+      [:prepared_cache, status] ->
+        Logger.debug("Prepared cache #{status} for query: #{inspect(measurements.query)}")
+
+      [:prepare_query, :start] ->
+        start = DateTime.from_unix!(measurements.system_time, :native)
+
+        Logger.debug(
+          "Started preparing query #{inspect(metadata.query)} at system_time: #{DateTime.to_string(start)}," <>
+            " ref: #{inspect(metadata.telemetry_span_context)}"
+        )
+
+      [:prepare_query, :stop] ->
+        duration = System.convert_time_unit(measurements.duration, :native, :millisecond)
+
+        Logger.debug(
+          "Finished preparing query  #{inspect(metadata.query)} in #{duration}ms" <>
+            " ref: #{inspect(metadata.telemetry_span_context)}"
+        )
+
+      [:prepare_query, :exception] ->
+        Logger.error(
+          "An exception occcured while preparing  #{inspect(metadata.query)}, kind: #{metadata.kind}" <>
+            "reason: #{inspect(metadata.reason)}, stacktrace: #{metadata.stacktrace}"
+        )
+
+      [:execute_query, :start] ->
+        start = DateTime.from_unix!(measurements.system_time, :native)
+
+        Logger.debug(
+          "Started executing query  #{inspect(metadata.query)} at system_time: #{DateTime.to_string(start)}," <>
+            " ref: #{inspect(metadata.telemetry_span_context)}"
+        )
+
+      [:execute_query, :stop] ->
+        duration = System.convert_time_unit(measurements.duration, :native, :millisecond)
+
+        Logger.debug(
+          "Finished executing query  #{inspect(metadata.query)} in #{duration}ms" <>
+            " ref: #{inspect(metadata.telemetry_span_context)}"
+        )
+
+      [:execute_query, :exception] ->
+        Logger.error(
+          "An exception occcured while executing  #{inspect(metadata.query)}, kind: #{metadata.kind}" <>
+            "reason: #{inspect(metadata.reason)}, stacktrace: #{metadata.stacktrace}"
+        )
+    end
   end
 end
