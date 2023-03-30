bdf9186f85f906b8a72f7d56eab0778790dc20eb
Harun Zengin <h.zengin@sonnen.de>
2023-03-30 14:09:45 +0200

    add docs and default handler for telemetry

--- lib/xandra/telemetry.ex
+++ lib/xandra/telemetry.ex
@@ -6,6 +6,49 @@ defmodule Xandra.Telemetry do
   metrics and events. Below we list all the possible events emitted by Xandra, alongside
   their measurements and metadata.
 
+  ### Xandra Connection
+
+  * `[:xandra, :connection]` and `[:xandra, :disconnection]`
+
+    * Measurements:
+
+    * Metadata:
+      * `:connection_name` - given name of the connection or `nil`
+      * `:host` - the host of the node the connection is connected to
+      * `:port` - the port of the node the connection is connected to
+      * `:reason` - reason of disconnection
+
+  * `[:xandra, :prepare_query, :start]` and `[:xandra, :prepare_query, :stop]`
+
+    * Measurements:
+      * `:system_time` on `:start`
+      * `:duration` on `:stop`
+
+    * Metadata:
+      * `:query` - the `t:Xandra.Prepared.t/0` query
+      * `:connection_name` - given name of the connection or `nil`
+      * `:host` - the host of the node the connection is connected to
+      * `:port` - the port of the node the connection is connected to
+      * `:reason` - if error, reason
+
+  * `[:xandra, :execute_query, :start]` and `[:xandra, :execute_query, :stop]`
+
+    * Measurements:
+      * `:system_time` on `:start`
+      * `:duration` on `:stop`
+
+    * Metadata:
+      * `:query` - the `t:Xandra.Simple.t/0` or `t:Xandra.Batch.t/0` query
+      * `:connection_name` - given name of the connection or `nil`
+      * `:host` - the host of the node the connection is connected to
+      * `:port` - the port of the node the connection is connected to
+      * `:reason` - if error, reason
+
+  * `[:xandra, :prepared_cache, :hit]` and `[:xandra, :prepared_cache, :miss]`
+
+    * Measurements:
+      * `:query` - the `t:Xandra.Prepared.t/0` query
+
   ### Warnings
 
     * Event name: `[:xandra, :server_warnings]`
@@ -22,4 +65,30 @@ defmodule Xandra.Telemetry do
         `t:Xandra.Prepared.t/0`, or `t:Xandra.Simple.t/0`
 
   """
+
+  require Logger
+
+  def attach_default_handler() do
+    events = [
+      [:xandra, :connection],
+      [:xandra, :disconnection],
+      [:xandra, :prepared_cache, :hit],
+      [:xandra, :prepared_cache, :miss],
+      [:xandra, :prepare_query, :start],
+      [:xandra, :prepare_query, :stop],
+      [:xandra, :execute_query, :start],
+      [:xandra, :execute_query, :stop]
+    ]
+
+    :telemetry.attach_many(
+      "xandra-default-telemetry-handler",
+      events,
+      &__MODULE__.handle_event/4,
+      :no_config
+    )
+  end
+
+  def handle_event([:xandra | event], measurements, metadata, :no_config) do
+    :ok
+  end
 end
