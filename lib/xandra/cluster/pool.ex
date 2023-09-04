e6370aec9729238a3d21e9e2673acc6771de7ea5
Harun Zengin <h.zengin@sonnen.de>
2023-09-04 14:19:16 +0200

    Implement suggested changes

--- lib/xandra/cluster/pool.ex
+++ lib/xandra/cluster/pool.ex
@@ -153,24 +153,25 @@ defmodule Xandra.Cluster.Pool do
     queue_before_connecting_opts = Keyword.fetch!(cluster_opts, :queue_before_connecting)
     queue_before_connecting_timeout = Keyword.fetch!(queue_before_connecting_opts, :timeout)
 
-    data = %__MODULE__{
-      pool_options: pool_opts,
-      contact_nodes: Keyword.fetch!(cluster_opts, :nodes),
-      load_balancing_module: lb_mod,
-      load_balancing_state: lb_mod.init(lb_opts),
-      autodiscovered_nodes_port: Keyword.fetch!(cluster_opts, :autodiscovered_nodes_port),
-      xandra_mod: Keyword.fetch!(cluster_opts, :xandra_module),
-      control_conn_mod: Keyword.fetch!(cluster_opts, :control_connection_module),
-      target_pools: Keyword.fetch!(cluster_opts, :target_pools),
-      name: Keyword.get(cluster_opts, :name),
-      pool_supervisor: pool_sup,
-      refresh_topology_interval: Keyword.fetch!(cluster_opts, :refresh_topology_interval),
-      reqs_before_connecting: %{
-        queue: :queue.new(),
-        max_size: Keyword.fetch!(queue_before_connecting_opts, :buffer_size)
-      },
-      sync_connect_ref: sync_connect_ref_or_nil && {parent, sync_connect_ref_or_nil}
-    }
+    data =
+      %__MODULE__{
+        pool_options: pool_opts,
+        contact_nodes: Keyword.fetch!(cluster_opts, :nodes),
+        load_balancing_module: lb_mod,
+        load_balancing_state: lb_mod.init(lb_opts),
+        autodiscovered_nodes_port: Keyword.fetch!(cluster_opts, :autodiscovered_nodes_port),
+        xandra_mod: Keyword.fetch!(cluster_opts, :xandra_module),
+        control_conn_mod: Keyword.fetch!(cluster_opts, :control_connection_module),
+        target_pools: Keyword.fetch!(cluster_opts, :target_pools),
+        name: Keyword.get(cluster_opts, :name),
+        pool_supervisor: pool_sup,
+        refresh_topology_interval: Keyword.fetch!(cluster_opts, :refresh_topology_interval),
+        reqs_before_connecting: %{
+          queue: :queue.new(),
+          max_size: Keyword.fetch!(queue_before_connecting_opts, :buffer_size)
+        },
+        sync_connect_ref: sync_connect_ref_or_nil && {parent, sync_connect_ref_or_nil}
+      }
 
     actions = [
       {:next_event, :internal, :start_control_connection},
@@ -414,16 +415,11 @@ defmodule Xandra.Cluster.Pool do
 
     # Find all connected hosts
     connected_hosts =
-      query_plan
-      |> Enum.map(fn %Host{} = host -> Map.fetch(data.peers, Host.to_peername(host)) end)
-      |> Enum.map(fn
-        {:ok, %{pool_pid: pid, host: host}} ->
-          {pid, host}
-
-        _other ->
-          nil
-      end)
-      |> Enum.reject(&is_nil/1)
+      for host <- query_plan,
+        %{pool_pid: pid, host: host} = Map.get(data.peers, Host.to_peername(host)),
+        not is_nil(host),
+        is_pid(pid),
+        do: {pid, host}
 
     reply =
       case connected_hosts do
@@ -435,7 +431,8 @@ defmodule Xandra.Cluster.Pool do
   end
 
   defp handle_host_added(%__MODULE__{} = data, %Host{} = host) do
-    data = update_in(data.load_balancing_state, &data.load_balancing_module.host_added(&1, host))
+    data =
+      update_in(data.load_balancing_state, &data.load_balancing_module.host_added(&1, host))
 
     data =
       update_in(
--- test/xandra/cluster_test.exs
+++ test/xandra/cluster_test.exs
@@ -156,7 +156,8 @@ defmodule Xandra.ClusterTest do
 
       cluster = TestHelper.start_link_supervised!({Xandra.Cluster, opts})
 
-      assert {:ok, %Xandra.Page{}} = Xandra.Cluster.execute(cluster, "SELECT * FROM system.local")
+      assert {:ok, %Xandra.Page{}} =
+        Xandra.Cluster.execute(cluster, "SELECT * FROM system.local")
 
       assert {:ok, %Xandra.Page{}} =
                Xandra.Cluster.execute(
