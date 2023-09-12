ac909211b8d89a91b6fa6d144643cfd00b7d7ed4
Andrea Leopardi <an.leopardi@gmail.com>
2023-09-12 23:21:06 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -105,7 +105,7 @@ defmodule Xandra.Mixfile do
       {:ex_doc, "~> 0.28", only: :dev},
       {:excoveralls, "~> 0.17", only: :test},
       {:mox, "~> 1.0", only: :test},
-      {:stream_data, "~> 0.5.0", only: [:dev, :test]},
+      {:stream_data, "~> 0.6.0", only: [:dev, :test]},
       {:nimble_lz4, "~> 0.1.3", only: [:dev, :test]}
     ]
   end
