8d171b0416a8c39da931f7b4c31aee18ec03d2bd
Andrea Leopardi <an.leopardi@gmail.com>
2023-10-03 18:33:35 +0200

    WIP

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Xandra.Mixfile do
     [
       app: :xandra,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
