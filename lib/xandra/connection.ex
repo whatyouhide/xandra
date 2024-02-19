0bac294d0f151c231c9c0e3043505d5d24fba687
Harun Zengin <h.zengin@sonnen.de>
2024-02-19 19:43:02 +0100

    release stream id

--- lib/xandra/connection.ex
+++ lib/xandra/connection.ex
@@ -519,6 +519,7 @@ defmodule Xandra.Connection do
   end
 
   def disconnected(:cast, {:release_stream_id, stream_id}, %__MODULE__{} = data) do
+    data = update_in(data.free_stream_ids, &MapSet.put(&1, stream_id))
     data = update_in(data.in_flight_requests, &Map.delete(&1, stream_id))
     {:keep_state, data}
   end
@@ -629,7 +630,6 @@ defmodule Xandra.Connection do
   def connected(:cast, {:timed_out_id, stream_id}, %__MODULE__{} = data) do
     data = update_in(data.in_flight_requests, &Map.delete(&1, stream_id))
     data = update_in(data.timed_out_ids, &MapSet.put(&1, stream_id))
-
     {:keep_state, data}
   end
 
